2d9334ac66a4df193a41840d443731cfc2ac1dc3
KarinXimenaGuerrero <k.guerreroh@uniandes.edu.co>
2024-02-24 18:28:10 -0500

    cambios

--- App/model.py
+++ App/model.py
@@ -332,6 +332,6 @@ def evalRatings(book1, book2):
 
 def sortBooks(catalog):
     # TODO completar los cambios del return para el sort lab 5 (Parte 1).
-    sorted_books = catalog["booksublist"]
+    sorted_books = catalog["books"]
     sorted_books = sort_algorithm.sort(sorted_books, evalRatings)
     return sorted_books
